457283740923295389423bcff76b5770a346115f
Jonas Almeida <jalmeida@mathbiol.org>
2019-07-02 10:47:35 -0400

    loading ui

--- roc.js
+++ roc.js
@@ -3,5 +3,21 @@ console.log('online tool available at https://episphere.github.io/roc');
 
 
 (function(){
+    const roc={}
+
+    roc.ui=function(div){
+        // what div is this about?
+        div = div||document.createElement('div')
+        if(typeof(div)=='string'){div=document.getElementById(div)}
+        roc.div=div
+        console.log('assembling UI at ',div)
+        return div
+    }
+
+    if(typeof(define)!="undefined"){
+        define(roc)
+    }else if(typeof(window)!="undefined"){
+        window.roc=roc
+    }
 
 })()
