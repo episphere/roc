659e1fe95caa09e985e5bb1b1377fbadd4df3d9b
Jonas Almeida <jalmeida@mathbiol.org>
2023-08-06 10:55:33 -0400

    adjusting observed label

--- roc.js
+++ roc.js
@@ -11,7 +11,7 @@ roc.ui=function(div){ // called onload by the reference web application
     // populate div
     // note demo points to D1 test data set produced as per https://cran.r-project.org/web/packages/plotROC/vignettes/examples.html
     let h = '<h3>Data</h3>'
-    h +='<p>Provide data in two columns, [observed (0/1),predicted (numeric)] (<a href="?D1.csv">demo1</a>, <a href="?D2.csv">demo2</a>, borrowed from R\'s <a href="http://www.biosoft.hacettepe.edu.tr/easyROC/" target="_blank">easyROC</a> . Predicted is a number, typically between 0 and 1 indicating the cumulative probablity of a positive prediction, but can just as well be any number that evolves monotonically with the positive prediction. It can be, for example, the activation value of the output of a neural network.</p>'
+    h +='<p>Provide data in two columns, [observed (0/1),predicted (numeric)], or try with test data <a href="?D1.csv">demo1</a> and <a href="?D2.csv">demo2</a> borrowed from R\'s <a href="http://www.biosoft.hacettepe.edu.tr/easyROC/" target="_blank">easyROC</a>. Predicted is a number, typically between 0 and 1 indicating the cumulative probablity of a positive prediction, but can just as well be any number that evolves monotonically with the positive prediction. It can be, for example, the activation value of the output of a neural network.</p>'
     h +='<table><tr><td>'
     h +='<textarea id="rocData" style="height:500px;width:150px;font-size:small"></textarea>'
     h +='</td><td id="rocTd" style="vertical-align:top"><div id="plotDiv">(ROC will be ploted here)</div></td><td id="confusion">...</td></tr></table>'
@@ -54,8 +54,8 @@ roc.ui=function(div){ // called onload by the reference web application
     <h5 style="color:black">Confusion table</h5>
     <p>Segmentation: <input id="segValue" size=10></p>
     <table>
-    <tr><td></td><td align="middle">Predicted</td></tr>
-    <tr><td style="transform: rotate(-90deg);vertical-align:bottom">Observed<br><span style="font-size:xx-small">&nbsp;</span></td><td>
+    <tr><td></td><td align="left:middle">Predicted</td></tr>
+    <tr><td id="observedLabel" style="transform: rotate(-90deg);vertical-align:bottom;max-width:3em">Observed<br>&nbsp;</td><td>
         <table style="border:solid">
         <tr><td id="totalCount" align="center" style="border:solid">total</td><td align="center">true</td><td align="center">false</td></tr>
         <tr><td align="right">true</td><td style="background-color:silver" align="center" id="truePos">true positives</td><td style="background-color:silver" align="center" id="falseNeg">false negatives</td></tr>
@@ -147,7 +147,6 @@ roc.plotDiv=(div)=>{
             mirror: true,
             fixedrange: true,
             showspikes: true,
-
           },
           yaxis: {
             title: 'true positive rate',
